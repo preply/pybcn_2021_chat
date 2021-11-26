d52d2bb6fc6cb7997f059e12d9d8e76417f6cdc2
iurii.boiko <iurii.boiko@preply.com>
2021-11-26 15:52:26 +0100

    Socket fix

--- app/chat/views.py
+++ app/chat/views.py
@@ -20,19 +20,18 @@ manager = ConnectionManager()
 @router.websocket("/ws/{room_id}")
 async def websocket_endpoint(
     room_id: str,
-    user_id: str,
     websocket: WebSocket,
     db: Session = Depends(deps.get_db),
 ):
-    user = UserCRUD(db).get(user_id)
     await manager.connect(user=user, websocket=websocket)
     try:
         while True:
-            data = await websocket.receive_text()
+            data = await websocket.receive_json()
+            user = UserCRUD(db).get(data["userId"])
             message = RoomCRUD(db).add_message(
                 room_id=room_id,
                 user_id=user.id,
-                text=data,
+                text=data["message"],
                 lang=user.lang,
             )
             await manager.broadcast(message=message)
